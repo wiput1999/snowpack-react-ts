7cc1b9b79f4145a52bb7aeebca03dc76ec9cd1be
Wiput Pootong <wiput.pootong@gmail.com>
2021-01-16 02:40:21 +0700

    fix: baseUrl

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,5 +9,8 @@ module.exports = {
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
   ],
+  buildOptions: {
+    baseUrl: 'https://wiput1999.github.io/snowpack-react-ts/'
+  },
   routes: [{ match: 'routes', src: '.*', dest: '/index.html' }],
 };
